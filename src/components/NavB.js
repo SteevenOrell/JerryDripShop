769d2c6f4ddf98322d1db9b32b47388c1574d046
SteevenOrell <nunkediesteevenorelljunior.wemin@georgebrown.ca>
2020-12-29 17:58:51 -0500

    v 0.1

--- src/components/NavB.js
+++ src/components/NavB.js
@@ -23,12 +23,7 @@ class NavB extends React.Component {
     }
 
 
-    onToggleClick(){
-
-
-        var searchBtn = document.getElementById("btn1");
-        
-      }
+  
   
   
 
@@ -38,17 +33,16 @@ class NavB extends React.Component {
 
     render() { 
         return ( <>
-        
-          <div id="divNavBar">
-        <Navbar id="FirstNavBar" collapseOnSelect expand="lg" >
-  <Navbar.Brand href="#home" ><img src={logo} id="logo"/></Navbar.Brand>
 
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" id="toggleMenu" onClick={()=>{this.onToggleClick()}} />
+<Navbar id="FirstNavBar" collapseOnSelect expand="lg" >
+  <Navbar.Brand href="#home"  ><img src={logo} id="imglogo" /></Navbar.Brand>
+
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" id="toggleMenu"  />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
      
    
-    <NavDropdown title={<span className="colorNavB" > Men</span>}  className="middleLinks" 
+    <NavDropdown title={<span className="colorNavB" > Men</span>}  className="middleLinks" id="basic-nav-dropdown"
             onMouseEnter={(e) => this.setState({ dropOpen1: true })}
             onMouseLeave={(e) => this.setState({ dropOpen1: false })}  show={this.state.dropOpen1}>
         <NavDropdown.Item href="#action/3.1"  >Clothing</NavDropdown.Item>
@@ -56,7 +50,7 @@ class NavB extends React.Component {
         <NavDropdown.Item href="#action/3.4" >Accessories</NavDropdown.Item>
       </NavDropdown>
 
-      <NavDropdown title={<span className="colorNavB" >Women</span>}  className="middleLinks" 
+      <NavDropdown title={<span className="colorNavB" >Women</span>}  className="middleLinks" id="basic-nav-dropdown"
             onMouseEnter={(e) => this.setState({ dropOpen2: true })}
             onMouseLeave={(e) => this.setState({ dropOpen2: false })} show={this.state.dropOpen2} >
         <NavDropdown.Item href="#action/3.1"  >Clothing</NavDropdown.Item>
@@ -64,7 +58,7 @@ class NavB extends React.Component {
         <NavDropdown.Item href="#action/3.4" >Accessories</NavDropdown.Item>
       </NavDropdown>
 
-      <NavDropdown title={<span className="colorNavB" >Kids</span>}  className="middleLinks"
+      <NavDropdown title={<span className="colorNavB" >Kids</span>}  className="middleLinks" id="basic-nav-dropdown"
             onMouseEnter={(e) => this.setState({ dropOpen3: true })}
             onMouseLeave={(e) => this.setState({ dropOpen3: false })} show={this.state.dropOpen3} >
         <NavDropdown.Item href="#action/3.1"  >Clothing</NavDropdown.Item>
@@ -72,37 +66,29 @@ class NavB extends React.Component {
         <NavDropdown.Item href="#action/3.4" >Accessories</NavDropdown.Item>
       </NavDropdown>
 
-      <Nav.Link href="#SignIn-Up" className="middleLinks"><span className="colorNavB"> Sign in / Register </span></Nav.Link>
+          <Nav.Link href="#SignIn-Up" className="middleLinks"><span className="colorNavB"> Sign in / Register </span></Nav.Link>
 
-      <NavDropdown title={<span className="colorNavB" >Customer Care</span>}  className="middleLinks" 
-            onMouseEnter={(e) => this.setState({ dropOpen4: true })}
-            onMouseLeave={(e) => this.setState({ dropOpen4: false })}  show={this.state.dropOpen4} >
-        <NavDropdown.Item href="#OrderStatus"  >Order Status</NavDropdown.Item>
-        <NavDropdown.Item href="#ContactUs" className="dropdownItem">Contact us</NavDropdown.Item>
-        <NavDropdown.Item href="Return" className="dropdownItem">Return</NavDropdown.Item>
-        <NavDropdown.Item href="#PrivacyPolicy" className="dropdownItem">Privacy Policy</NavDropdown.Item>
-        <NavDropdown.Item href="TermofSale" className="dropdownItem">Term of Sale</NavDropdown.Item>
-        <NavDropdown.Item href="TermofUse" className="dropdownItem">Term of Use</NavDropdown.Item>
-      </NavDropdown>
+<NavDropdown title={<span className="colorNavB" >Customer Care</span>}  className="middleLinks"  id="basic-nav-dropdown"
+      onMouseEnter={(e) => this.setState({ dropOpen4: true })}
+      onMouseLeave={(e) => this.setState({ dropOpen4: false })}  show={this.state.dropOpen4} >
+  <NavDropdown.Item href="#OrderStatus"  >Order Status</NavDropdown.Item>
+  <NavDropdown.Item href="#ContactUs" className="dropdownItem">Contact us</NavDropdown.Item>
+  <NavDropdown.Item href="Return" className="dropdownItem">Return</NavDropdown.Item>
+  <NavDropdown.Item href="#PrivacyPolicy" className="dropdownItem">Privacy Policy</NavDropdown.Item>
+  <NavDropdown.Item href="TermofSale" className="dropdownItem">Term of Sale</NavDropdown.Item>
+  <NavDropdown.Item href="TermofUse" className="dropdownItem">Term of Use</NavDropdown.Item>
+</NavDropdown>
 
-      <Nav.Link href="#AboutUs" className="middleLinks"><span className="colorNavB">About Us</span></Nav.Link>
-    
-    </Nav>
-    <Nav id="leftNavBtn">  
-    <Sidebar id="Sidebar" visible={this.state.visible} position="top" baseZIndex={1000000} onHide={() => this.setState({ visible: false })}>
-                        <div>
-                        <h1> What are you looking for?</h1>
-                        <form>
-                        <input type="text" id="searchInput" placeholder="Enter Keywords"/> <br></br><br></br>
-                        <button id="searchBtn" onClick={() => this.setState({ visibleTop: false })}  > Search </button>
-                        </form>
-                        </div>
-                    </Sidebar>
+<Nav.Link href="#AboutUs" className="middleLinks"><span className="colorNavB">About Us</span></Nav.Link>
+
+</Nav>
+<Nav id="leftNavBtn">  
 
 
 
-      <button  id="btn1" 
-       onClick={(e) => this.setState({visible:true})}> <i className="pi pi-search" id="searchIcon"> </i> </button>
+
+<button  id="btn1" 
+ onClick={(e) => this.setState({visible:true})}> <i className="pi pi-search" id="searchIcon"> </i> </button>
 
 
 
@@ -111,12 +97,18 @@ class NavB extends React.Component {
 <button id="shoppingBag" > <img src={shoppingBag} /> </button>
 
 
-    </Nav>
-  </Navbar.Collapse>
+</Nav>
+</Navbar.Collapse>
 </Navbar>
-        
-        
-</div>
+<Sidebar id="Sidebar" visible={this.state.visible} position="top" baseZIndex={1000000} onHide={() => this.setState({ visible: false })}>
+                  <div>
+                  <h1> What are you looking for?</h1>
+                  <form>
+                  <input type="text" id="searchInput" placeholder="Enter Keywords"/> <br></br><br></br>
+                  <button id="searchBtn" onClick={() => this.setState({ visibleTop: false })}  > Search </button>
+                  </form>
+                  </div>
+              </Sidebar>
         </> );
     }
 }
